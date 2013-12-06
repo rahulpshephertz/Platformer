9f6249cbd0163f2acb32161509ac6f2e009958d6
Rahul Panchal <errahulpanchal@gmail.com>
2013-12-06 12:53:20 +0530

    Api keys

--- Platformer/GlobalContext.cs
+++ Platformer/GlobalContext.cs
@@ -11,8 +11,8 @@ namespace Platformer
 {
     public class GlobalContext
     {
-        public static String API_KEY = "5790d3d3185b90ca9a4a59123f2f3a78ec9e82a1113a988c44ea29af5fbb3923";
-        public static String SECRET_KEY = "5b02b4111d251dd2864a37ac272a9578fdf41a16953cb21be792c90b3fa39599";
+        public static String API_KEY = "";
+        public static String SECRET_KEY = "";
         public static ServiceAPI SERVICE_API = null;
         public static String AccessToken = null;
         public static String facebookAppId = "1412246962345063";
